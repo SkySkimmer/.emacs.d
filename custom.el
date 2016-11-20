3636beee7e22f770ae549bf09c427fa814124781
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-11-20 20:27:11 +0100

    custom.el update

--- custom.el
+++ custom.el
@@ -6,35 +6,57 @@
  '(safe-local-variable-values
    (quote
     ((eval progn
-	   (let
-	       ((coq-root-directory
-		 (when buffer-file-name
-		   (locate-dominating-file buffer-file-name ".dir-locals.el")))
-		(coq-project-find-file
-		 (and
-		  (boundp
-		   (quote coq-project-find-file))
-		  coq-project-find-file)))
-	     (set
-	      (make-local-variable
-	       (quote tags-file-name))
-	      (concat coq-root-directory "TAGS"))
-	     (setq camldebug-command-name
-		   (concat coq-root-directory "dev/ocamldebug-coq"))
-	     (unless coq-project-find-file
-	       (set
-		(make-local-variable
-		 (quote compile-command))
-		(concat "make -C " coq-root-directory))
-	       (set
-		(make-local-variable
-		 (quote compilation-search-path))
-		(cons coq-root-directory nil)))
-	     (when coq-project-find-file
-	       (setq default-directory coq-root-directory))))))))
+           (let
+               ((m31-root-directory
+                 (when buffer-file-name
+                   (locate-dominating-file buffer-file-name ".dir-locals.el")))
+                (m31-project-find-file
+                 (and
+                  (boundp
+                   (quote m31-project-find-file))
+                  m31-project-find-file)))
+             (when m31-root-directory
+               (setq tags-file-name
+                     (concat m31-root-directory "TAGS"))
+               (add-to-list
+                (quote compilation-search-path)
+                m31-root-directory)
+               (if
+                   (not m31-project-find-file)
+                   (setq compile-command
+                         (concat "make -C " m31-root-directory))))
+             (setq m31-executable
+                   (concat m31-root-directory "andromeda.native"))))
+     (eval progn
+           (let
+               ((coq-root-directory
+                 (when buffer-file-name
+                   (locate-dominating-file buffer-file-name ".dir-locals.el")))
+                (coq-project-find-file
+                 (and
+                  (boundp
+                   (quote coq-project-find-file))
+                  coq-project-find-file)))
+             (set
+              (make-local-variable
+               (quote tags-file-name))
+              (concat coq-root-directory "TAGS"))
+             (setq camldebug-command-name
+                   (concat coq-root-directory "dev/ocamldebug-coq"))
+             (unless coq-project-find-file
+               (set
+                (make-local-variable
+                 (quote compile-command))
+                (concat "make -C " coq-root-directory))
+               (set
+                (make-local-variable
+                 (quote compilation-search-path))
+                (cons coq-root-directory nil)))
+             (when coq-project-find-file
+               (setq default-directory coq-root-directory))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
