b8259e18b7d49a0adb0fbaaed13f223c41144e95
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2016-11-30 15:13:39 +0100

    Fix package https use

--- init.el
+++ init.el
@@ -1,8 +1,12 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
 
+(setq tls-checktrust t)
+(setq gnutls-verify-error t)
+
 (require 'package)
 (setq package-enable-at-startup nil)
+(setq package-archives (list '("gnu" . "https://elpa.gnu.org/packages/")))
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
