ec1a2f7e22cd3b31610c91fe24c8969c0f943472
Gaetan Gilbert <gaetan.gilbert@ens-lyon.fr>
2017-04-20 17:18:59 +0200

    custom noise

--- custom.el
+++ custom.el
@@ -8,7 +8,8 @@
     (ivy-hydra wgrep-ag wgrep hydra company-coq company nv-delete-back rainbow-mode counsel-projectile projectile systemd dired ws-butler which-key use-package undo-tree tuareg try org-bullets markdown-mode magit highlight-symbol flycheck-ocaml expand-region counsel auctex ace-window)))
  '(safe-local-variable-values
    (quote
-    ((coq-prog-args "-emacs" "-boot")
+    ((coq-prog-name . "../HoTT-master//hoqtop")
+     (coq-prog-args "-emacs" "-boot")
      (eval let
            ((default-directory
               (locate-dominating-file buffer-file-name ".dir-locals.el")))
