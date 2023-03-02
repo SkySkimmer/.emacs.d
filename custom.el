de61c77b9620b4d000951c80efa30d0c98f0fc32
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2023-03-02 18:42:37 +0100

    Correct font setting
    set-frame-font does nonsense in daemon mode

--- custom.el
+++ custom.el
@@ -232,4 +232,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "1ASC" :family "Liberation Mono"))))
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
